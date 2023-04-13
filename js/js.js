aa4cdd2e4d315e7b2ec667d98967809503007273
RobCec06 <robin.cecconi@epitech.eu>
2023-04-13 17:18:34 +0200

    adding new template on css

--- js/js.js
+++ js/js.js
@@ -1,3 +1,11 @@
-console.log('Hello World');
-console.error('This is an error');
-console.warn('This is a warning');
+function showHiddenText(element) {
+    const hiddenText = element.querySelector('.hidden-text');
+    hiddenText.style.display = 'block';
+    hiddenText.style.opacity = '1';
+}
+
+function hideHiddenText(element) {
+    const hiddenText = element.querySelector('.hidden-text');
+    hiddenText.style.display = 'none';
+    hiddenText.style.opacity = '0';
+}
