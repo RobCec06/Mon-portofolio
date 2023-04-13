48e5042359294703ca93d718a2a62e059305419c
RobCec06 <robin.cecconi@epitech.eu>
2023-04-13 20:56:36 +0200

    Adding footer

--- js/js.js
+++ js/js.js
@@ -1,11 +1,11 @@
 function showHiddenText(element) {
-    const hiddenText = element.querySelector('.hidden-text');
+    const hiddenText = element.parentElement.querySelector('.hidden-text');
     hiddenText.style.display = 'block';
     hiddenText.style.opacity = '1';
 }
 
 function hideHiddenText(element) {
-    const hiddenText = element.querySelector('.hidden-text');
+    const hiddenText = element.parentElement.querySelector('.hidden-text');
     hiddenText.style.display = 'none';
     hiddenText.style.opacity = '0';
 }
